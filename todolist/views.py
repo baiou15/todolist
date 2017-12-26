c808783a67002851221325db0cdde5573e19e609
baiou15 <ou.bai.dartmouth@gmail.com>
2017-12-25 20:23:47 -0500

    add url, view

--- todolist/views.py
+++ todolist/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+
+def index(request):
+    message = "Current time: {}".format(datetime.datetime.now())
+    return HttpResponse(message)
