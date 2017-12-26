873d0a22d2b1c3c559d4de744520f0811b9b5bb9
baiou15 <ou.bai.dartmouth@gmail.com>
2017-12-26 18:02:20 -0500

    modify urls, views

--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,33 @@
-from django.http import HttpResponse
-import datetime
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 
-def index(request):
-    message = "Current time: {}".format(datetime.datetime.now())
-    return HttpResponse(message)
+class Todo:
+
+    def __init__(self, description, isCompleted):
+        self.title = description
+        self.completed = isCompleted
+
+
+def todolist(request):
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'add':
+            title = request.POST.get('title')
+            Todo.objects.create(title=title)
+
+    todolist = Todo.objects.all()
+    return render(request, 'todolist.html', locals())
+
+
+def delete(request, pk):
+    todo = get_object_or_404(Todo, id=pk)
+    todo.delete()
+    return HttpResponseRedirect('/')
+
+
+def complete(request, pk):
+    todo = get_object_or_404(Todo, id=pk)
+    todo.completed = True
+    todo.save()
+    return HttpResponseRedirect('/')
